ed59335bfa365f5739412ef1c606c4ced7c0df46
gamingfail123 <lekahade@gmail.com>
2017-08-25 23:15:30 +0200

    some fixes :P

---
+++ update_pack_windows.bat
@@ -0,0 +1,5 @@
+@echo off
+echo "You can only update your Resourcepack using this method if you have Git installed."
+echo "You can get Git here: https://git-scm.com/"
+git reset --hard
+git pull origin master
