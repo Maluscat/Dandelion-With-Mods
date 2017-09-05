c55de6b614b973e0c33b3e15eb27c106e73f6165
Roman Gräf <roman.graef@gmail.com>
2017-09-05 16:33:22 +0200

    Auto-git download

--- update_pack_linux.sh
+++ update_pack_linux.sh
@@ -1,5 +1,63 @@
 #!/bin/bash/
-echo "You can only update your Resourcepack using this method if you have Git installed."
-echo "You can get Git here: https://git-scm.com/"
-git reset --hard
-git pull origin master
+# Installs git by checking for apt-get or yum
+installgit() {
+    yum install git-core
+    if [ ! $? -eq 0 ]; then 
+        apt-get update
+        apt-get install git-all
+        if [ ! $? -eq 0 ]; then
+            echo "Failed to install git."
+            echo "Please download manually from https://git-scm.com/"
+            exit 1;
+        fi
+    fi   
+}
+
+# Checks out if there if no .git information
+checkoutrepo() {
+    git clone git@github.com:Hallo89/Dandelion-With-Mods.git .
+    if [ ! $? -eq 0 ]; then
+        echo "An error occured during clone".
+        exit 1
+    fi
+}
+
+updatelocal() {
+    git clone 
+    git fetch --all
+    git reset --hard origin/master
+}
+
+# Running git --version to check whether git is installed
+git --version
+
+# Check for error code
+if [ ! $? -eq 0 ]; then
+    echo "You do not have git installed!"
+    echo "You need git to download this update!"
+    while true; do
+        read -p "Want to install it? (Y/N)" yn
+        case $yn in 
+          [Yy]* ) installgit; break;;
+          [Nn]* ) echo "If you dont want to"; exit 1;;
+          * ) echo "Please answer (Y/N)";;
+         esac          
+    done
+fi
+
+# Check current repo
+git remote show origin
+if [ ! $? -eq  ]; then 
+    echo "This isn't registered as git repository."
+    while true; do
+        read -p "Clone anyway? (Y/N) " yn
+        case $yn in
+          [Yy]* ) checkoutrepo; break;;
+          [Nn]* ) echo "If you dont want to"; exit 1;;
+          * ) echo "Please answer (Y/N)";;
+        esac          
+    done
+fi
+
+# No errors or pitfalls detected
+updatelocal
