04375f0131a755bc2316a3f60a292f4338af3c69
Roman Gräf <roman.graef@gmail.com>
2017-09-07 14:32:45 +0200

    Added debug messages to `update_mod_list.bat`

--- update_mod_list.bat
+++ update_mod_list.bat
@@ -1,8 +1,12 @@
 @echo off
 echo Searching for mods!
+echo Found mods:
 dir /B assets
+echo Now writing to mods.md
 echo ^|Modname^| > mods.md
 echo ^|---^| >> mods.md
 for /F %%A IN ('dir /B assets') DO (
 	echo ^|%%A^| >> mods.md
 )
+echo Done.
+pause >nul
