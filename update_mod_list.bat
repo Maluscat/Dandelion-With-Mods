4e97738b9f9ec194ed54d3332483df341af56e99
Hallo89 <karl.leiser@yahoo.com>
2017-09-10 22:25:45 +0200

    Add ##Modlist to the modlist.md
    I have the fear I broke something NotLikeThis

--- update_mod_list.bat
+++ update_mod_list.bat
@@ -3,7 +3,8 @@ echo Searching for mods!
 echo Found mods:
 dir /B assets
 echo Now writing to mods.md
-echo ^|Modname^| > mods.md
+echo ## Modlist > mods.md
+echo ^|Modname^| >> mods.md
 echo ^|---^| >> mods.md
 for /F %%A IN ('dir /B assets') DO (
 	echo ^|%%A^| >> mods.md
