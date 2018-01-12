d41aa110ce94984ff3ce77787f218827d6a8ba27
Hallo89 <karl.leiser@yahoo.com>
2018-01-12 22:22:28 +0100

    Winter textures!
    Saved here because I most likely won't still use them this winter, and I know my habits of accidently deleting stuff.
    Now go, onward, and rust!

--- update_mod_list.sh
+++
@@ -1,9 +0,0 @@
-cd assets
-echo "Updating mod list!"
-echo "Searching for mods..."
-printf '%s\n' `/usr/bin/ls`
-echo "Now redirecting!"
-echo "## Modlist" > ../mods.md
-echo "|Modname|" >> ../mods.md
-echo "|---|" >> ../mods.md
-printf '|%s|\n' `ls | tr "/" " "` >> ../mods.md
--- update_pack_windows.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo "You can only update your Resourcepack using this method if you have Git installed."
-echo "You can get Git here: https://git-scm.com/"
-git reset --hard
-git pull origin master
